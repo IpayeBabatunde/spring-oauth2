7cdfa284e1924430748ea086ea0ec02e3a681d2e
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-12-30 03:54:45 +0100

    init commit

---
+++ src/main/java/com/ipaye/springoauth2/HelloController.java
@@ -0,0 +1,13 @@
+package com.ipaye.springoauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Insta, Insta.com";
+    }
+
+}
